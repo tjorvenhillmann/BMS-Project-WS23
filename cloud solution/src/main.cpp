6b2452868ede0fe49a418a30501e3f901250b597
Tjorven Hillmann <tjorven.hillmann@web.de>
2023-04-21 08:29:38 +0200

    Test script for pushing data to InfluxDB

--- cloud solution/src/main.cpp
+++ cloud solution/src/main.cpp
@@ -1,9 +1,105 @@
 #include <Arduino.h>
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
+// WiFi AP SSID
+#define WIFI_SSID ""
+// WiFi password
+#define WIFI_PASSWORD ""
+// InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
+// InfluxDB 1.8+  (v2 compatibility API) server url, e.g. http://192.168.1.48:8086
+#define INFLUXDB_URL "https://eu-central-1-1.aws.cloud2.influxdata.com"
+// InfluxDB v2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+// InfluxDB 1.8+ (v2 compatibility API) use form user:password, eg. admin:adminpass
+#define INFLUXDB_TOKEN "3MKqIhDWG-MiVgwLJMo2Cd9VrT4JfTof0_LKKrd6xauNHPoWkrudm6P7QaaR1t5MJ0B_gJw409xt1yJLjJMR1w=="
+// InfluxDB v2 organization name or id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+// InfluxDB 1.8+ (v2 compatibility API) use any non empty string
+#define INFLUXDB_ORG "Cloud-Solution/81721c757fe2e372"
+// InfluxDB v2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+// InfluxDB 1.8+ (v2 compatibility API) use database name
+#define INFLUXDB_BUCKET "ESP32"
+
+// Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+// Examples:
+//  Pacific Time: "PST8PDT"
+//  Eastern: "EST5EDT"
+//  Japanesse: "JST-9"
+//  Central Europe: "CET-1CEST,M3.5.0,M10.5.0/3"
+//  Central Europe: Berlin  	CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3" 
+
+// InfluxDB client instance with preconfigured InfluxCloud certificate
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+
+// Data point
+Point sensor("wifi_status");
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Setup wifi
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Add tags
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Alternatively, set insecure connection to skip server certificate validation 
+  //client.setInsecure();
+
+  // Accurate time is necessary for certificate validation and writing in batches
+  // For the fastest time sync find NTP servers in your area: https://www.pool.ntp.org/zone/
+  // Syncing progress and the time will be printed to Serial.
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("rssi", WiFi.RSSI());
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 10s
+  Serial.println("Wait 10s");
+  delay(10000);
 }
