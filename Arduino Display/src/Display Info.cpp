439a6988f99886d63bb89c765458d77af2d92d8e
DESKTOP-AOAPPCN\Ramón Fürst <ramon.fuerst@gmail.com>
2023-05-12 11:19:50 +0200

    CAN COMM Implemented and working

--- Arduino Display/src/Display Info.cpp
+++ Arduino Display/src/Display Info.cpp
@@ -9,9 +9,14 @@
 
 #include <LCDWIKI_GUI.h> //Core graphics library
 #include <LCDWIKI_KBV.h> //Hardware-specific library
-#include <math.h> // Math library
+#include <math.h> //MATH
+#include <SPI.h> //CAN Comm
+#include <mcp2515.h> //CAN Comm
 
 LCDWIKI_KBV mylcd(ILI9486,A3,A2,A1,A0,A4); //model,cs,cd,wr,rd,reset
+
+struct can_frame canMsg;
+MCP2515 mcp2515(49); // CS PIN from MCP CAN Module
 
 //define some colour values
 #define BLACK   0x0000
@@ -23,24 +28,46 @@ LCDWIKI_KBV mylcd(ILI9486,A3,A2,A1,A0,A4); //model,cs,cd,wr,rd,reset
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
+//define Frame ids
+int resolution = 100;
+byte frame_id_1 = 0x001;  // temperature ID
+byte frame_id_2 = 0x002;  // Voltage ID
+byte frame_id_3 = 0x003;  // Current SOH
+byte frame_id_4 = 0x004;  // Balance Status
+byte hexmsg[2] = {0,0};
+
 //Functions
 void clearSOC (); //clears all segments
 void setSOC(); //sets the 1-5 green segments
 bool SOCchange(); //returns true if the SOC changed
 
+void getCANmessage();
+float byte2float();
+
 //Global variable
-int SOCold = 0;
-int SOC = 0;
+int SOCold = 0; // old SOC
+float vol_all = 0; //total voltage
+int power = 0; //power
+
+float temp_list[4];
+float vol_list[4];
+float current_soc_soh_list[4];
+int balance_list[4];
+
 
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(9600); //Debugging
+  //CAN
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  //LCD
   mylcd.Init_LCD();
   mylcd.Fill_Screen(BLACK);
   mylcd.Invert_Display(1); //Needed in Order for the colours to be correct
-    mylcd.Set_Text_Mode(0); //Aktiv sein für irgendwelche Stringkacke?
-  //mylcd.Fill_Screen(0x0000);
+  mylcd.Set_Text_Mode(0); //Aktiv sein für irgendwelche Stringkacke?
   mylcd.Set_Rotation(1); //Horizontal Usage 
 
     /*
@@ -87,6 +114,53 @@ void setup()
   mylcd.Print_String("Cell3:     V", 230, 260);
   mylcd.Print_String("Cell4:     V", 230, 290);
 
+}
+
+float byte2float(byte * var, int resolution){
+  //static int resolution = 100; //ggfs. init danach
+  return ((float) *reinterpret_cast<int*>(var))/resolution;
+}
+
+void getCANmessage(){
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    if (canMsg.can_id == frame_id_1)
+    {
+      for (int i = 0; i<canMsg.can_dlc; i+=2)  {  // print the data
+      //Serial.print("Temp: ");
+      hexmsg[1] = canMsg.data[i];
+      hexmsg[0] = canMsg.data[i+1];
+      temp_list[i/2] = byte2float(hexmsg, 100);
+      //Serial.println(temp_list[i/2]);
+      }  
+    }
+    else if (canMsg.can_id == frame_id_2)
+    {
+      for (int i = 0; i<canMsg.can_dlc; i+=2)  {  // print the data
+      //Serial.print("Voltage: ");
+      hexmsg[1] = canMsg.data[i];
+      hexmsg[0] = canMsg.data[i+1];
+      vol_list[i/2] = byte2float(hexmsg, 100);
+      //Serial.println(vol_list[i/2]);
+      }  
+    }
+    else if (canMsg.can_id == frame_id_3)
+    {
+      for (int i = 0; i<canMsg.can_dlc; i+=2)  {  // print the data
+      //Serial.println("Current & SOH & SOC & CAPACITY: ");
+      hexmsg[1] = canMsg.data[i];
+      hexmsg[0] = canMsg.data[i+1];
+      current_soc_soh_list[i/2] = byte2float(hexmsg, 100);
+      //Serial.println( current_soc_soh_list[i]);
+      }      
+    }
+    else if (canMsg.can_id == frame_id_4)
+    {
+      for (int i = 0; i<canMsg.can_dlc; i+=1)  {  // print the data
+      //Serial.print("Balance Status: ");
+      //Serial.println(balance_list[i], HEX);
+      }      
+    }
+  }
 }
 
 void clearSOC() {
@@ -167,13 +241,14 @@ else {
 void loop() 
 {
   //VALUES 
+  getCANmessage();
 
   //Quadrant 1 (Current, Temp, Power)
-
+  power = round((vol_list[0] + vol_list[1] + vol_list[2] + vol_list[3]) * current_soc_soh_list[0]); 
   mylcd.Set_Text_colour(WHITE);
-  mylcd.Print_Number_Int(50, 125, 5, 2, ' ', 10); //Temp
-  mylcd.Print_Number_Float(9.9, 1, 125, 35, '.', 2, ' '); //Current
-  mylcd.Print_Number_Int(65, 125, 65, 2, ' ', 10); //Power 
+  mylcd.Print_Number_Int(round(temp_list[1]), 125, 5, 2, ' ', 10); //Temp
+  mylcd.Print_Number_Float(current_soc_soh_list[0], 1, 125, 35, '.', 2, ' '); //Current
+  mylcd.Print_Number_Int(power, 125, 65, 2, ' ', 10); //Power 
 
   //Quadrant 2 (Cloud, State)
 
@@ -208,21 +283,23 @@ void loop()
 
   mylcd.Set_Text_colour(WHITE);
   mylcd.Print_String("SOC", 30, 150);
-  SOC++;
-  if (SOCchange(SOC) == true) {
-    setSOC(SOC);
+  if (SOCchange(current_soc_soh_list[1]) == true) {
+    setSOC(current_soc_soh_list[1]);
   }
   
 
   //Quadrant 4 (Voltage, Cell voltage)
 
   mylcd.Set_Text_colour(WHITE);
-  
-  mylcd.Print_Number_Float(11.2, 1, 380, 150, '.', 2, ' '); //Total Voltage
-  mylcd.Print_Number_Float(3.88, 2, 340, 200, '.', 2, ' '); //Cell 1
-  mylcd.Print_Number_Float(3.87, 2, 340, 230, '.', 2, ' '); //Cell 2
-  mylcd.Print_Number_Float(3.86, 2, 340, 260, '.', 2, ' '); //Cell 3
-  mylcd.Print_Number_Float(3.85, 2, 340, 290, '.', 2, ' '); //Cell 4
+  vol_all = vol_list[0] + vol_list[1] + vol_list[2] + vol_list[3]; //Total voltage 11.20 V
 
+  mylcd.Print_Number_Float(vol_all, 1, 380, 150, '.', 2, ' '); //Total Voltage
+  mylcd.Print_Number_Float(vol_list[0], 2, 340, 200, '.', 2, ' '); //Cell 1
+  mylcd.Print_Number_Float(vol_list[1], 2, 340, 230, '.', 2, ' '); //Cell 2
+  mylcd.Print_Number_Float(vol_list[2], 2, 340, 260, '.', 2, ' '); //Cell 3
+  mylcd.Print_Number_Float(vol_list[3], 2, 340, 290, '.', 2, ' '); //Cell 4
+  
+
+  
   delay(1000); //Aktualisierungsdelay
 }
