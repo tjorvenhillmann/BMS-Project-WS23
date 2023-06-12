2cf11860e0dd76127380844b8b417c9df0b57e0a
LAPTOP-P991IFJB\g-mha <g-m.haase@gmx.de>
2023-06-12 19:42:17 +0200

    SW & HW test #1

--- BMS/src/main.cpp
+++ BMS/src/main.cpp
@@ -8,9 +8,9 @@ bool startingup = true;
 bool error = false; 
 unsigned long prev_time = 0; 
 float prev_voltage = 0; 
-float soc = 25.50; // values from 0 to 100
-float soh = 0; 
-float rul = 0; 
+float soc; // values from 0 to 100
+float soh; 
+float rul; 
 const float balThreshold = 0.030; // threshold for balancing [V]
 const float stopBalThreshold = 0.005; // threshold for stopping [V]
 bool charging = false; // indicates charging status: charging = true, discharging = false
@@ -33,21 +33,21 @@ int voltage_status[4] = {0, 0, 0, 0}; // voltage within balancing threshold to c
                                       // 1: high voltage; -1: low voltage 
 
 // measurement data 
-float temp_1 = 7.70; 
-float temp_2 = 10.15; 
-float temp_3 = 20.20; 
-float temp_4 = 25.20; 
-float cell_1_V = 2.90; 
-float cell_2_V = 3.22; 
-float cell_3_V = 3.75; 
-float cell_4_V = 3.59; 
-float current = 4.50; 
+float temp_1; 
+float temp_2; 
+float temp_3; 
+float temp_4; 
+float cell_1_V; 
+float cell_2_V; 
+float cell_3_V; 
+float cell_4_V; 
+float current; 
 
 // output data 
-bool balance_status_1 = true; 
+bool balance_status_1 = false; 
 bool balance_status_2 = false;
 bool balance_status_3 = false; 
-bool balance_status_4 = true; 
+bool balance_status_4 = false; 
 bool battery_switch = false;
 
 // safety limits (read-only)
@@ -61,7 +61,7 @@ const float nom_voltage = 3.6; // [V]
 const float nom_capacity = 1500.0; // [mAh]
 const float stopChargingCurrent = 0.1; // [A]
 const float charging_cutoff_current = 0.75; // current maximum [A] 
-const float discharging_cutoff_current = -18.0; // negative current maximum [A] 
+const float discharging_cutoff_current = -10.0; // negative current maximum [A] 
 const long charge_time = 150.0*60.0*1000.0; // maximum charging time [ms]
 
 // global variables for limits
@@ -204,6 +204,7 @@ void checkCurrent_withACS712(){
   float Amp = ((sensVoltage - offset)/0.066); // [A], sensor measures 66 mV/A
   
   current = Amp; // [A]
+  current_soc_soh[0] = current; // Update for CAN
 
  // check current limits 
   if(current >= charging_cutoff_current){
@@ -231,6 +232,12 @@ void checkVoltage(){
   cell_3_V = cell_3_sensValue * (v_ref/1023.0);
   cell_4_V = cell_4_sensValue * (v_ref/1023.0); 
 
+  // Update for CAN-Message
+  vol_list[0] = cell_1_V; 
+  vol_list[1] = cell_2_V; 
+  vol_list[2] = cell_3_V; 
+  vol_list[3] = cell_4_V; 
+
  
   //check for every value
   float cell_V[] = {cell_1_V, cell_2_V, cell_3_V, cell_4_V}; 
@@ -240,7 +247,7 @@ void checkVoltage(){
     // Status Check
     if(cell_V[i] >= cutoff_voltage_upper_limit-balThreshold){ // on upper limit 
       voltage_status[i] = 1; 
-    }else if(cell_V[i] >= cutoff_voltage_lower_limit+balThreshold){ // on lower limit 
+    }else if(cell_V[i] <= cutoff_voltage_lower_limit+balThreshold){ // on lower limit 
       voltage_status[i] = -1; 
     }else{
       voltage_status[i] = 0; 
@@ -285,6 +292,10 @@ void checkTemp(){
     }
   } 
 
+  temp_list[0] = temp_C[0];
+  temp_list[1] = temp_C[1]; 
+  temp_list[2] = temp_C[2];   
+  temp_list[3] = temp_C[3];    
 }
 
 void controlBalancing(){
@@ -303,6 +314,8 @@ void controlBalancing(){
     if(cell_V[i] > maxVol){
       maxVol = cell_V[i];
       maxVol_index = i; 
+      Serial.println("maxVol_index einlesen");
+      Serial.println(maxVol_index);
     }
   }
   // find minimum voltage
@@ -317,20 +330,25 @@ void controlBalancing(){
   // Balancing control based on cell voltage difference
   // calculate difference
   volDiff = maxVol - minVol; 
-  Serial.println(volDiff);
+  // Serial.println(volDiff);
   // enable balancing of cell with highest voltage if necessary
   if(volDiff > balThreshold){
     balance_status[maxVol_index] = true;
-    Serial.println(balance_status_1);
+    Serial.print("balance status max Vol: "); 
+    Serial.print(balance_status[maxVol_index]);
+    Serial.print("\n"); 
+    Serial.println("0 entry!!!");
   }
   // disable balancing of cell with lowest voltage 
   if(balance_status[minVol_index] == true){
     balance_status[minVol_index] = false; 
+    Serial.println("1 entry!!!");
   }
   // disable balancing of all cells, if balanced
   if(volDiff < stopBalThreshold){
     for(i=0; i<j; i++){
       balance_status[i] = false;
+      Serial.println("2 entry!!!");
     }
   }
 
@@ -338,10 +356,12 @@ void controlBalancing(){
   for(i=0; i<j; i++){
     if(voltage_status[i] == 1){ // voltage on upper limit -> enable 
       balance_status[i] = true; 
+      Serial.println("3 entry!!!");
     }
   
     if(voltage_status[i] == -1){ // voltage on lower limit -> disable
       balance_status[i] = false; 
+      Serial.println("4 entry!!!");
     }
   }
 
@@ -351,9 +371,18 @@ void controlBalancing(){
   balance_status_3 = balance_status[2];
   balance_status_4 = balance_status[3];
 
+  digitalWrite(BALANCE_STATUS_1_PIN, balance_status[0]);
+  digitalWrite(BALANCE_STATUS_2_PIN, balance_status[1]);
+  digitalWrite(BALANCE_STATUS_2_PIN, balance_status[2]);
+  digitalWrite(BALANCE_STATUS_2_PIN, balance_status[3]);
+
+  // Set to CAN-Message
+  balance_error_list[0] = balance_status[0];
+  balance_error_list[1] = balance_status[1];
+  balance_error_list[2] = balance_status[2];
+  balance_error_list[3] = balance_status[3];
+
   Serial.print("Balance Control: ");
-  Serial.print(balance_status[maxVol_index]);
-  Serial.print("\t");
   Serial.print(balance_status_1);
   Serial.print("\t");
   Serial.print(balance_status_2);
@@ -361,6 +390,29 @@ void controlBalancing(){
   Serial.print(balance_status_3);
   Serial.print("\t");
   Serial.print(balance_status_4);
+  Serial.print("\n");
+  Serial.print("Balance Control array: ");
+  Serial.print(balance_status[0]);
+  Serial.print("\t");
+  Serial.print(balance_status[1]);
+  Serial.print("\t");
+  Serial.print(balance_status[2]);
+  Serial.print("\t");
+  Serial.print(balance_status[3]);
+  Serial.print("\n");
+  Serial.print("Balancing Triggers: ");
+  Serial.print("minVol: ");
+  Serial.print(minVol);
+  Serial.print("\t");
+  Serial.print("maxVol: ");
+  Serial.print(maxVol);
+  Serial.print("\t");
+  Serial.print("Difference: ");
+  Serial.print(volDiff); 
+  Serial.print("\n");
+  Serial.print(minVol_index);
+  Serial.print("\t");
+  Serial.print(maxVol_index);
   Serial.print("\n");
 }
 
@@ -386,11 +438,12 @@ void battery_state(){
   // SOC
   float new_soc = soc - 100 * charge_change/(nom_capacity/1000.0); 
   soc = max(0.0, min(100.0, new_soc)); 
+  current_soc_soh[1] = soc; // Update for CAN
 
   // SOH 
   float new_soh = 100 * (medium_vol/cutoff_voltage_upper_limit) * (soc/100); 
   soh = max(0.0, min(100.0, new_soh)); 
-
+  current_soc_soh[2] = soh; // Update for CAN
   // RUL
   float new_rul = 100 * (1- (soc/100)) * (soh/100); 
   rul = max(0.0, min(100.0, new_rul)); 
@@ -413,9 +466,14 @@ void adjust_temp_limits(){
 void connectBattery(){ // to ensure, that only connects when safe
   if(error){
     battery_switch = false; 
+    digitalWrite(BATTERY_SWITCH_PIN, LOW); 
   }else{
     battery_switch = true; 
+    digitalWrite(BATTERY_SWITCH_PIN, HIGH); 
   }
+  Serial.print("Battery Switch");
+  Serial.print(battery_switch); 
+  Serial.print("\n");
 }
 
 
@@ -434,8 +492,8 @@ void updateCANmessages(){
   can_counter=0;  
   for(int i = 0; i < (canMsg1.can_dlc/2); i++){ // TEMPERATURE
   float2byte(temp_list[i],tmsg,resolution);
-  Serial.print(tmsg[1],HEX);
-  Serial.println(tmsg[0],HEX);
+  // Serial.print(tmsg[1],HEX);
+  // Serial.println(tmsg[0],HEX);
   canMsg1.data[can_counter] = tmsg[1];   
   canMsg1.data[can_counter+1] = tmsg[0];
   can_counter = can_counter + 2;
@@ -443,8 +501,8 @@ void updateCANmessages(){
   can_counter=0; 
   for(int i = 0; i < (canMsg2.can_dlc/2); i++){ // VOLTAGE
   float2byte(vol_list[i],tmsg,resolution);
-  Serial.print(tmsg[1],HEX);
-  Serial.println(tmsg[0],HEX);
+  // Serial.print(tmsg[1],HEX);
+  // Serial.println(tmsg[0],HEX);
   canMsg2.data[can_counter] = tmsg[1];   
   canMsg2.data[can_counter+1] = tmsg[0];
   can_counter = can_counter + 2;
@@ -452,8 +510,8 @@ void updateCANmessages(){
   can_counter=0; 
   for(int i = 0; i < (canMsg3.can_dlc/2); i++){ // Current, SOC, SOH
   float2byte(current_soc_soh[i],tmsg,resolution);
-  Serial.print(tmsg[1],HEX);
-  Serial.println(tmsg[0],HEX);
+  // Serial.print(tmsg[1],HEX);
+  // Serial.println(tmsg[0],HEX);
   canMsg3.data[can_counter] = tmsg[1];   
   canMsg3.data[can_counter+1] = tmsg[0];
   can_counter = can_counter + 2;
@@ -472,10 +530,13 @@ void updateCANmessages(){
 
 void sendCANmessages(){
   mcp2515.sendMessage(&canMsg1);  // CAN Message for Temperatures
+  delay(100); 
   mcp2515.sendMessage(&canMsg2);  // CAN Message for Voltages
+  delay(100); 
   mcp2515.sendMessage(&canMsg3);  // CAN Message for Current SOC SOH
+  delay(100); 
   mcp2515.sendMessage(&canMsg4);  // CAN Message for Balance Error Status
-  //delay(100);
+  // delay(100);
 }
 
 
@@ -525,9 +586,17 @@ void loop() {
                     || voltage_fault[2] != 0 || voltage_fault[3] != 0 || temp_fault != 0){
     error = true;
     battery_switch = false;
+    digitalWrite(BATTERY_SWITCH_PIN, LOW); 
   }else{
     error = false; 
   }
+  balance_error_list[4] = error;
+
+  // Connect battery if everything okay
+  connectBattery(); 
+  Serial.print("Battery Switch: "); 
+  Serial.print(battery_switch); 
+  Serial.print("\n"); 
 
   startingup = false;
 
